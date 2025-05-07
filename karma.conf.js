0356c01df5247c7ebaee16bd9e3835213f64bb24
Dixita Kothari <155532001+dixitarite@users.noreply.github.com>
2025-05-07 11:02:24 +0530

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -7,28 +7,34 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage'),
+      require('karma-junit-reporter'), // Add this plugin for JUnit XML report
       require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false // Leave Jasmine Spec Runner output visible in browser
     },
     coverageReporter: {
       dir: require('path').join(__dirname, './coverage/angular.io-example'),
-      subdir: 'lcov-report',
+      subdir: 'coverage',
       reporters: [
-        { type: 'html' },         // generates ./coverage/angular.io-example/lcov-report/index.html
+        { type: 'html' },  // Generates ./coverage/angular.io-example/coverage/index.html
         { type: 'text-summary' },
         { type: 'lcovonly' },
-        { type: 'json-summary' }  // needed for GitHub Actions to enforce coverage thresholds
+        { type: 'json-summary' }  // Needed for GitHub Actions to enforce coverage thresholds
       ]
     },
-    reporters: ['progress', 'kjhtml', 'coverage'],
+    reporters: ['progress', 'kjhtml', 'coverage', 'junit'], // Add 'junit' here
+    junitReporter: {
+      outputDir: './test-results',  // Directory where XML report will be saved
+      outputFile: 'test-results.xml',  // Name of the XML file
+      useBrowserName: false  // Optionally, include the browser name in the XML file name
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
